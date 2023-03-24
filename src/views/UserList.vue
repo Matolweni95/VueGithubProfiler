faf1602589e0075dbc94ffe31fb66e20eb7adea8
Masixole Matolweni <72172920+Matolweni95@users.noreply.github.com>
2023-03-24 09:21:11 +0200

    Update UserList.vue
    chatbot attempt#2

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -109,8 +109,10 @@ created: function(){
 
 };
 
+</script>
 
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -120,9 +122,8 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-
-
 </script>
+<!--End of Tawk.to Script-->
 
 <style>
 
