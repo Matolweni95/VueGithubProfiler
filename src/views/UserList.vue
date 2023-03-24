1aeae751b419d543a5f6d00df79bfdfc45d328f8
Masixole Matolweni <72172920+Matolweni95@users.noreply.github.com>
2023-03-24 09:22:25 +0200

    Update UserList.vue
    chatbot script removed

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -111,20 +111,6 @@ created: function(){
 
 </script>
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/641d4bfb4247f20fefe7b340/1gs971kgp';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-
 <style>
 
 img {
